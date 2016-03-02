375f62a554772982f89ac27e790270118de83a7d
Sebastian Gierłowski <ebondust@gmail.com>
2016-03-02 21:57:37 +0100

    CPU Implementation Final

--- Chip8/CPU.cs
+++ Chip8/CPU.cs
@@ -8,5 +8,58 @@ namespace Chip8
 {
     class CPU
     {
+        protected ushort opcode;
+        protected byte[] memory = new byte[4096];
+        protected byte[] register = new byte[16];
+        protected ushort I;//index register
+        protected ushort pc;
+
+        protected byte[] gfx = new byte[64 * 32];
+
+        protected byte delay_timer;
+        protected byte sound_timer;
+        protected ushort[] stack = new ushort[16];
+        protected ushort sp = 0;
+        protected byte[] keypad = new byte[16];
+
+        protected void push(ushort value)
+        {
+            stack[sp] = value;
+            sp++;
+        }
+        protected ushort pop()
+        {
+            sp--;
+            ushort p = stack[sp];
+            return p;
+        }
+
+        public void initialize()
+        {
+
+            pc = 0x200;
+            opcode = 0;
+            I = 0;
+            sp = 0;
+
+            for (int i = 0; i < 16; i++)  // Clear stack and registers V0-VF
+            {
+                stack[i] = 0;
+                register[i] = 0;
+            }
+
+            for (int i = 0; i < 4096; i++)// Clear memory
+                memory[i] = 0;
+
+            for (int i = 0; i < 64; i++)// Clear display
+                for (int j = 0; j < 32; j++)
+                {
+                    gfx[i * j] = 0;
+                }
+
+            // Reset timers
+            delay_timer = 0;
+            sound_timer = 0;
+        }
     }
 }
