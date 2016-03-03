7ec2c439d7f06c953418e04deb8b96bbae076c88
Sebastian Gierłowski <ebondust@gmail.com>
2016-03-03 22:31:49 +0100

    Emulator class template

--- Chip8/EmulatorCore.cs
+++ Chip8/EmulatorCore.cs
@@ -8,5 +8,35 @@ namespace Chip8
 {
     class EmulatorCore
     {
+        public void LoadProgram(string path)
+        {
+
+        }
+        public bool emulateCycle()
+        {
+            return true;
+        }
+
+
+        public void Input(int key)
+        {
+
+        }
+        public void endInput(int key)
+        {
+
+        }
+
+        private void _initialize_fontset()
+        {
+
+        }
+
+        public void drawGfx()
+        {
+
+        }
+
+
     }
 }
