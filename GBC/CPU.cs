8d3fa69eeeefb56ca2f3d6ac4ff4219441f90494
Sebastian Gierłowski <ebondust@gmail.com>
2016-05-23 02:29:23 +0200

    Added the z80 cpu structure.

--- GBC/CPU.cs
+++ GBC/CPU.cs
@@ -8,5 +8,36 @@ namespace GBC
 {
     class CPU
     {
+        public byte A; // general purporse registers
+        public byte B;
+        public byte C;
+        public byte D;
+        public byte E;
+        public byte H;
+        public byte L;
+        public byte F; // Flags register.
+        public ushort IC; // Instructions counter register.
+        public byte SP; // stack pointer register.
+
+        public ushort BC
+        {
+            get
+            {
+                ushort b = (ushort)(B << 8);
+                ushort c = (ushort)(C);
+                return (ushort)(b | c);
+            }
+
+            set
+            {
+                B = (byte)(value >> 8);
+                C = (byte)(value);
+            }
+        }
+
+
+        // z80 procesor timers.
+        public int m = 0;
+        public int t = 0;
     }
 }
