392f6b343fabd23eb6da337aa8690fa01d099d46
Sebastian Gierłowski <ebondust@gmail.com>
2016-05-24 01:21:50 +0200

    Begining of MMU implementation

--- GBC/MMU.cs
+++ GBC/MMU.cs
@@ -8,5 +8,17 @@ namespace GBC
 {
     class MMU
     {
+        ///<summary> memory managment unit </summary>
+        public Stack<byte> stack = new Stack<byte>();
+
+        public ushort IP; // Instruction pointer.
+        bool inBios = true;
+
+        // memory regions
+        byte[] bios; // bios
+        byte[] rom; // rom banks
+        byte[] wram; // working ram
+        byte[] eram; // external ram
+        byte[] zram; // zero-page ram, high speed memory
     }
 }
